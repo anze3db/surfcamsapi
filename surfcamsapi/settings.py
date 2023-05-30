f57b28bfe8fdd56545328eaeda28b66172c99053
Anže Pečar <anze@pecar.me>
2023-05-30 15:12:51 +0200

    Add SECRET KEY

--- surfcamsapi/settings.py
+++ surfcamsapi/settings.py
@@ -32,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-nkvp43du*gc*kfsc2!^g9+xwv-hd-q$kjg*l3p#^d+96@s@8l0"
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
