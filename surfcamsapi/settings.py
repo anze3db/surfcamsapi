875f8f39c6069fded4279b559e04c9310904d00d
Anže Pečar <anze@pecar.me>
2023-05-31 01:42:52 +0200

    Fix .env file loading

--- surfcamsapi/settings.py
+++ surfcamsapi/settings.py
@@ -10,8 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+from pathlib import Path
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 import environ
 
+# Set up environment variables
 env = environ.Env(
     # set casting, default value
     CSRF_COOKIE_DOMAIN=(str, None),
@@ -21,12 +27,7 @@ env = environ.Env(
     SENTRY_DNS=(str, ""),
     APP_NAME=(str, "local"),
 )
-
-from pathlib import Path
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
+environ.Env.read_env(BASE_DIR / ".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -101,12 +102,7 @@ STATIC_ROOT = BASE_DIR / "staticroot"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+DATABASES = {"default": env.db()}
 
 
 # Password validation
