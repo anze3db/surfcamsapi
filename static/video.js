606a6304c3eb623e77dda1e8e3ccaefc24168f44
Anže Pečar <anze@pecar.me>
2023-06-13 13:46:51 +0100

    Minor improvements

--- static/video.js
+++ static/video.js
@@ -1,16 +1,26 @@
 class Video {
-    constructor(url) {
+    constructor(url, backUrl) {
         this.url = url;
+        this.backUrl = backUrl;
         this.video = document.getElementById('video');
     }
     setSize() {
-        video.setAttribute('width', document.body.clientWidth);
+        if (this.video.videoHeight > window.innerHeight) {
+            this.video.setAttribute('height', window.innerHeight);
+        }
+        else {
+            this.video.setAttribute('width', document.body.clientWidth);
+        }
     }
     addListeners() {
         window.addEventListener('resize', this.setSize);
         this.video.addEventListener('play', () => {
             this.setSize();
+
         });
+        this.video.addEventListener('click', () => {
+            window.location = this.backUrl;
+        })
     }
     loadSource() {
         if (Hls.isSupported()) {
