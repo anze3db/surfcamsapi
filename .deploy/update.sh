9bbeadb26f4a66375e35e1a7120edf9a8af73d0a
Anže Pečar <anze@pecar.me>
2024-11-10 23:20:50 +0000

    Update deploy settings

--- .deploy/update.sh
+++ .deploy/update.sh
@@ -5,6 +5,6 @@ git pull
 uv sync --locked
 uv run python manage.py collectstatic --noinput
 uv run python manage.py migrate
-ps axf | grep 'gunicorn: master \[surfcams\]' | awk '{print "sudo kill -hup " $1}' | sh
+sudo systemctl reload surfcams
 echo `date "+%Y-%m-%d %H:%M:%S.%3N"` ' Updated' >> update.log
 popd
